30283f0cda61cdbce3d40d2d4d39f0d72ed427c4
Carlos Alvarado <carlosalvaradom@icloud.com>
2023-03-08 00:04:45 -0500

    Initial commit

---
+++ src/main/java/co/edu/ucatolica/hisclinic/HisclinicApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucatolica.hisclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ucatolica/hisclinic/HisclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucatolica.hisclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
